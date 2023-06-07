8597053a0506ccb014d7fc38af7f7836427496e0
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-06-07 17:07:44 -0600

    WorkedOnDataSeeder

--- Newspaper/Models/Issue.cs
+++ Newspaper/Models/Issue.cs
@@ -11,5 +11,31 @@ namespace Newspaper.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public List<Article> Articles { get; set; } = new();
+
+        public int ArticleCount()
+        {
+            int articleCount = Articles.Count();
+            return articleCount;
+        }
+
+        public int ReporterCount()
+        {
+            List<string> reporters = new List<string>();
+            foreach(var article in Articles)
+            {
+                reporters.Add(article.Reporter.Name);
+            }
+            return reporters.Count;
+        }
+
+        public List<string> ReporterNames()
+        {
+            List<string> reporters = new List<string>();
+            foreach (var article in Articles)
+            {
+                reporters.Add(article.Reporter.Name);
+            }
+            return reporters;
+        }
     }
 }
